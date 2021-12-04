d2c1644faaf001a039ec6f0d861c24c1b48948b5
Gabor Soltesz-Keri <solteszkerigabor@outlook.hu>
2021-12-04 09:13:26 +0100

    Comments added to the code, to make it more understandable

--- whitenoise-demo.ts
+++ whitenoise-demo.ts
@@ -10,9 +10,11 @@ let squareH: number = 14;
 function draw(): void {
   for (let i: number = 0; i < 2000; i += squareH) {
     for (let j: number = 0; j < 1500; j += squareW) {
+      // Change the color in every turn
       let c: number = Math.random() * 255;
       let color: string = `rgb(${c}, ${c}, ${c})`;
 
+      // Draw a square
       ctx.beginPath();
       ctx.fillStyle = color;
       ctx.fillRect(j, i, squareW, squareH);
@@ -21,4 +23,5 @@ function draw(): void {
   }
 }
 
+//"Animation", it runs the draw function in every 100ms
 setInterval(draw, 100);
